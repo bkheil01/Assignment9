c14b85922e9f7f082bbc4ce2a50ec7d9fa4104b0
Brett Heil <bkheil01@louisville.edu>
2015-03-25 14:29:37 -0400

    Update BlogController.cs

--- Assignment9/Assignment9/Controllers/BlogController.cs
+++ Assignment9/Assignment9/Controllers/BlogController.cs
@@ -65,7 +65,7 @@ namespace Assignment9.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit([Bind(Include = "BlogID,Name,About,Image,Favorite,Last,Date")(Exclude = "Past,Past1,Past2,Past3,Past4")] Blog blog)
+        public ActionResult Edit([Bind(Include = "BlogID,Name,About,Image,Past,Past1,Past2,Past3,Past4,Favorite,Last,Date")] Blog blog)
         {
             if (ModelState.IsValid)
             {

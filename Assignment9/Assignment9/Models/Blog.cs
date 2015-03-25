ec191ac553c900ca0eda6a979c576e2e2abcdf2f
Brett Heil <bkheil01@louisville.edu>
2015-03-25 14:33:23 -0400

    Update Blog.cs

--- Assignment9/Assignment9/Models/Blog.cs
+++ Assignment9/Assignment9/Models/Blog.cs
@@ -8,18 +8,17 @@ namespace Assignment9.Models
 {
     public class Blog
     {
+        public string Past { get; set; }
+        public string Past1 { get; set; }
+        public string Past2 { get; set; }
+        public string Past3 { get; set; }
+        public string Past4 { get; set; }
         [Required]
         public int BlogID { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
         public string About { get; set; }
-        
-        public string Past { get; set; }
-        public string Past1 { get; set; }
-        public string Past2 { get; set; }
-        public string Past3 { get; set; }
-        public string Past4 { get; set; }
         [Required]
         public string Image { get; set; }
         [Required]
